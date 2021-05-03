bcaae6736498e6b24ce08199beebf243536aad13
Luiz Cláudio <lc.danella.dev@gmail.com>
2021-05-03 11:45:56 -0300

    Adicionado funcao calc usando eval

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,15 @@ function createCalc() {
 
     start() {
       this.clickButtons();
-      alert("Started!");
+      this.enterListener();
+    },
+
+    enterListener() {
+      document.addEventListener("keyup", (e) => {
+        if (e.key === "Enter") {
+          this.calc();
+        }
+      });
     },
 
     clickButtons() {
@@ -19,7 +27,26 @@ function createCalc() {
         if (targetElement.classList.contains("grid-item-del")) {
           this.delItem();
         }
+        if (targetElement.classList.contains("grid-item-calc")) {
+          this.calc();
+        }
       });
+    },
+
+    calc() {
+      let calc = this.inputData.value;
+
+      try {
+        calc = eval(calc);
+        if (!calc) {
+          alert("Conta inválida");
+          return;
+        }
+        this.inputData.value = calc;
+      } catch (e) {
+        alert("Conta inválida");
+        return;
+      }
     },
 
     delItem() {
